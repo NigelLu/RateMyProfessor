b9188f3560f3f10e431d43381d7deda299a7e0f1
NigelLu <nigellu@outlook.com>
2023-11-16 11:58:10 -0500

    init

---
+++ src/main/java/edu/nyu/ratemyprofessor/RateMyProfessorApplication.java
@@ -0,0 +1,13 @@
+package edu.nyu.ratemyprofessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateMyProfessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateMyProfessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/nyu/ratemyprofessor/RateMyProfessorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nyu.ratemyprofessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateMyProfessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
